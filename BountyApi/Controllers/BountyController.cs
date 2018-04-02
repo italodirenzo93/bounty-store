421ed18ce2dc6a23a6c0654fc17025f56fdd5140
Italo Di Renzo <italo@caddetails.com>
2018-04-02 10:04:36 -0400

    Added server-side validation

--- BountyApi/Controllers/BountyController.cs
+++ BountyApi/Controllers/BountyController.cs
@@ -37,9 +37,19 @@ namespace BountyApi.Controllers
         [HttpPost]
         public IActionResult Create([FromBody] Bounty bounty)
         {
+            // Check that serialization was successful
             if (bounty == null)
                 return BadRequest();
 
+            // Check if the client passed an Id
+            if (bounty.Id > 0)
+                ModelState.AddModelError("Id", "Model Ids are automatically generated. Do not pass an Id on creation.");
+
+            // Validate inputs
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            // Add bounty
             _context.Bounties.Add(bounty);
             _context.SaveChanges();
 
@@ -49,19 +59,31 @@ namespace BountyApi.Controllers
         [HttpPut("{id}")]
         public IActionResult Update(long id, [FromBody] Bounty bounty)
         {
-            if (bounty == null || bounty.Id != id)
+            // Check that serialization was successful
+            if (bounty == null)
                 return BadRequest();
 
+            // Check that the id parameter matches the model's id
+            if (bounty.Id != id)
+                ModelState.AddModelError("Id", "The Id passed does not match the Id of the bounty data.");
+
+            // Validate inputs
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            // Retrieve the entity to update
             var b = _context.Bounties.FirstOrDefault(x => x.Id == id);
             if (b == null)
                 return NotFound();
 
+            // Update data
             b.Name = bounty.Name;
             b.Description = bounty.Description;
             b.AliveReward = bounty.AliveReward;
             b.DeadReward = bounty.DeadReward;
             b.Captured = bounty.Captured;
 
+            // Persist
             _context.Bounties.Update(b);
             _context.SaveChanges();
             return new NoContentResult();
@@ -70,12 +92,12 @@ namespace BountyApi.Controllers
         [HttpDelete("{id}")]
         public IActionResult Delete(long id)
         {
+            // Retrieve the entity to remove
             var bounty = _context.Bounties.FirstOrDefault(x => x.Id == id);
             if (bounty == null)
-            {
                 return NotFound();
-            }
 
+            // Remove it
             _context.Bounties.Remove(bounty);
             _context.SaveChanges();
             return new NoContentResult();
--- BountyApi/Models/Bounty.cs
+++ BountyApi/Models/Bounty.cs
@@ -1,12 +1,27 @@
-﻿namespace BountyApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BountyApi.Models
 {
     public class Bounty
     {
         public long Id { get; set; }
+
+        [Required]
+        [MaxLength(75)]
         public string Name { get; set; }
+
+        [Required]
+        [MaxLength(1000)]
         public string Description { get; set; }
+
+        [Required]
+        [Range(0, 10000)]
         public decimal AliveReward { get; set; }
+
+        [Required]
+        [Range(0, 10000)]
         public decimal DeadReward { get; set; }
+
         public bool Captured { get; set; }
     }
 }
