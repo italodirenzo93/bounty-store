0e4f14768a35ce16d3021d1a44d8a332d35bd589
Italo Di Renzo <italodirenzo93@gmail.com>
2019-04-06 19:19:00 -0400

    Update API to use specific port and fix database seed error

--- BountyApi/Startup.cs
+++ BountyApi/Startup.cs
@@ -31,14 +31,13 @@ namespace BountyApi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, BountyContext context)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            var context = app.ApplicationServices.GetService<BountyContext>();
             CreateTestData(context);
 
             // Accept requests from our client app
